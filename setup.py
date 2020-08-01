b488522d75b70912b0bf06e7e10845d61e2393ad
Yagna Sathish <aysathish0916@gmail.com>
2020-08-02 03:33:43 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict if a client will subscribe to a term deposit based on marketing campaign',
+    author='aysathish',
+    license='MIT',
+)
